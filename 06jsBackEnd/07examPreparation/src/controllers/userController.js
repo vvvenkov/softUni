cee5d061c79e821be55b067eac7a686a2481444a
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-11-07 13:47:20 +0100

    Add login post action

--- 06jsBackEnd/07examPreparation/src/controllers/userController.js
+++ 06jsBackEnd/07examPreparation/src/controllers/userController.js
@@ -11,11 +11,19 @@ userController.post('/register', async (req, res) => {
     const userData = req.body;
 
     await userService.register(userData);
-    res.redirect('/');
+    res.redirect('/users/login');
 });
 
 userController.get('/login', (req, res) => {
     res.render('user/login')
 });
+
+userController.post('/login', (req, res) => {
+    const loginData = req.body;
+
+    // Call userService.login
+    res.redirect('/')
+
+})
 
 export default userController;
