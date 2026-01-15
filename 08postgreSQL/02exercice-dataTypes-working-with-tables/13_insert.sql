23f0f8ba82b07492528551304758f55502d8426f
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2026-01-15 13:50:43 +0100

    added new exercises

---
+++ 08postgreSQL/02exercice-dataTypes-working-with-tables/13_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	minions_info(name, code, task, banana, email, mood)
+VALUES 
+	('Mark','GKYA',	'Graphing Points', 265.265, 'mark@minion.com', 'happy'),
+	('Mel',	'HSK', 'Science Investigation',	54784.996, 'mel@minion.com', 'stressed'),
+	('Bob', 'HF', 'Painting', 35.652,	'bob@minion.com', 'happy'),
+	('Darwin', 'EHND', 'Create a Digital Greeting', 321.958, 'darwin@minion.com', 'relaxed');
