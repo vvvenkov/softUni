df1f817e6c54ffb08b833683b1740ad19b45a5f1
vvvenkov <venelin.venkov@protonmail.com>
2026-01-27 13:12:52 +0100

    Add new lectures

---
+++ 09devOps/04.automatedTesting/06-Exercises-Automated-Testing-Resources/6-Integration-NET/LibroConsoleAPI/LibroConsoleAPI/DataAccess/LibroDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace LibroConsoleAPI.DataAccess
+{
+    public class LibroDbContextFactory : IDesignTimeDbContextFactory<LibroDbContext>
+    {
+        public LibroDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<LibroDbContext>();
+
+            builder.UseInMemoryDatabase("InMemoryLibroDb");
+
+            return new LibroDbContext(builder.Options);
+        }
+    }
+}
